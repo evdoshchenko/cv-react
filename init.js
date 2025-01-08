9fb5187dd6834fc2c6f9b2733c283d251f4c4caa
evdoshchenko <uppdanilynx@gmail.com>
2025-01-08 17:48:54 -0500

    del init.js

--- init.js
+++
@@ -1,7 +0,0 @@
-// const fs = import('fs')
-
-// // fs.copyFileSync('.env.sample', '.env')
-
-// fs.copyFileSync('src/assets/', 'dist/assets')
-
-// // fs.mkdirSync('tmp/pgdata', { recursive: true })
